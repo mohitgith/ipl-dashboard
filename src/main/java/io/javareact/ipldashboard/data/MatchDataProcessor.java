9a86a9796c3fa9475261369121932c60e7fb1b47
Mohit Shrivastava <42516296+mohitgith@users.noreply.github.com>
2021-07-16 02:03:55 +0530

    added files for spring batch

--- src/main/java/io/javareact/ipldashboard/data/MatchDataProcessor.java
+++ src/main/java/io/javareact/ipldashboard/data/MatchDataProcessor.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.batch.item.ItemProcessor;
 
 import io.javareact.ipldashboard.model.Match;
